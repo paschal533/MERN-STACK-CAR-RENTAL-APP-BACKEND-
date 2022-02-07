c05440abe7f6ab59da28ace907c3f5b9bb1282fd
unknown <okwuosahpaschal@gmail.com>
2022-02-07 23:10:25 +0100

    second commit

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use("/api/bookings/", require("./routes/bookingsRoute"));
 
 //----------deployment-----------------
 
-__dirname = path.resolve();
+/*__dirname = path.resolve();
 
 if(process.env.NODE_ENV==="production") {
  app.use(express.static(path.join(__dirname,"/frontend/build")));
@@ -26,11 +26,7 @@ if(process.env.NODE_ENV==="production") {
  app.get('*',(req,res)=>{
    res.sendFile(path.resolve(__dirname,"frontend","build","index.html"));
  });
-}else{
-	app.get("/", (req, res)=>{
-      res.send("API is running..");
-	});
-}
+}else{*/
 
 
 //----------deployment-----------
